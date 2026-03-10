8d9ef70803052af8a249b4ef8ddf241eda8ae96b
ricocanuto <ricardomoraescanuto@gmail.com>
2026-03-10 20:59:56 -0300

    update-globalStyles

--- devburguer-interface/src/main.jsx
+++ devburguer-interface/src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import Login from './containers/login/index.jsx'
+import GlobalStyles from'./styles/globalstyles'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Login />
+    <GlobalStyles />  
   </StrictMode>,
 )
